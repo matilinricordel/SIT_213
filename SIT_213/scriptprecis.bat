7bbbee9bfb3fc80c304fe7d459bf4bea32cd9cd2
mauny <mauny@PC-EDGAR>
2023-10-11 09:29:05 +0200

    pas de modif

---
+++ SIT_213/scriptprecis.bat
@@ -0,0 +1,28 @@
+@echo off
+
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	NRZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	NRZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	RZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	RZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	NRZT	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	5	-form	NRZT	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	NRZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	NRZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	RZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	RZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	NRZT	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	7	-form	NRZT	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	NRZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	NRZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	RZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	RZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	NRZT	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	9	-form	NRZT	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	NRZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	NRZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	RZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	RZ	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	NRZT	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	11	-form	NRZT	-codeur
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	13	-form	NRZ	
+java	-cp	bin/	simulateur.Simulateur	-mess	100000	-snrpb	13	-form	NRZ	-codeur
