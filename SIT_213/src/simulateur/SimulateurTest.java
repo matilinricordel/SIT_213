9d369f386cc80f298584929e2b2c84f4afe69a45
David <David@PC-MK-2>
2023-10-11 17:33:31 +0200

    simuTests

--- SIT_213/src/simulateur/SimulateurTest.java
+++ SIT_213/src/simulateur/SimulateurTest.java
@@ -1,5 +1,12 @@
 package simulateur;
 
-public class SimulateurTest {
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-}
+@RunWith(Suite.class)
+@SuiteClasses({
+        SimulateurArgumentsTest.class,
+        SimulateurScenariosTest.class,
+})
+public class SimulateurTest {}
