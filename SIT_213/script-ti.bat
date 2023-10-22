5def32dbf649a8cf6f5898f4d0ccbab1007b23d0
mauny <mauny@PC-EDGAR>
2023-10-22 15:46:52 +0200

    script dedit

--- SIT_213/script-ti.bat
+++ SIT_213/script-ti.bat
@@ -1,14 +1,3 @@
 @echo off
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	1	1	1	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	2	1	2	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	5	1	5	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	10	1	10	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	20	1	20	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	40	1	40	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	80	1	80	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	100	1	100	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	150	1	150	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	200	1	200	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	300	1	300	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	400	1	400	0.5
-java	-cp	bin/	simulateur.Simulateur		-mess	10000	-codeur	-snrpb	10	-form	NRZ	-nbEch 	100	-ti	500	1	500	0.5
+java	-cp	bin/	simulateur.Simulateur	-s	-mess	0100000000	-form	NRZ	-nbEch	100	-ti	100	0.5
+java	-cp	bin/	simulateur.Simulateur	-s	-mess	00100000000000000000	-form	NRZ	-nbEch	100	-ti	200	0.5
